237612bcbb2dd95b686ec9cad42dd29152e8194a
Shreyas <c2825212004@gmail.com>
2026-02-06 19:02:54 +0000

    this website is just a showcase website , not the actual product, please

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/auto-categorize-files.ts';
-import '@/ai/flows/ai-search-files.ts';
-import '@/ai/flows/suggest-file-organization.ts';
